295758cdd74ffd2f26dceabe107717dbc822f849
Maik Knof <knofm@hs-weingarten.de>
2025-03-19 10:50:05 +0100

    cleanup

--- scripts/gpsr_voice_cloning.py
+++ scripts/gpsr_voice_cloning.py
@@ -18,53 +18,35 @@ class GPSR_Demo(Node):
         # Service client for set_grammar
         self.grammar_client = self.create_client(SetGrammar, '/vosk/set_grammar')
 
-    def send_tts_goal_sync(self, text, language='', speaker_wave='', wait_before_speaking=0.0):
+    def tts(self, text, language='', speaker_wav='', wait_before_speaking=0.0):
         goal_msg = TTSAction.Goal()
         goal_msg.text = text
         goal_msg.language = language
-        goal_msg.speaker_wave = speaker_wave
+        goal_msg.speaker_wav = speaker_wav
         goal_msg.wait_before_speaking = wait_before_speaking
 
         self.get_logger().info('Sending TTS goal...')
         self.tts_client.wait_for_server()
-        send_goal_future = self.tts_client.send_goal_async(goal_msg)
-        rclpy.spin_until_future_complete(self, send_goal_future)
-        goal_handle = send_goal_future.result()
-        if not goal_handle.accepted:
-            self.get_logger().error('TTS goal rejected.')
-            return None
-        result_future = goal_handle.get_result_async()
-        rclpy.spin_until_future_complete(self, result_future)
-        result = result_future.result().result
-        self.get_logger().info(f'TTS result: success={result.success}, message={result.message}')
-        return result
+        self.tts_client.send_goal_async(goal_msg)
+        
     
-    def send_tts_voice_clone_goal_sync(self, text, language='', speaker_wave='', wait_before_speaking=0.0):
+    def tts_voice_clone(self, text, language='en', speaker_wav='', wait_before_speaking=0.0):
         goal_msg = TTSAction.Goal()
         goal_msg.text = text
         goal_msg.language = language
-        goal_msg.speaker_wave = speaker_wave
+        goal_msg.speaker_wav = speaker_wav
         goal_msg.wait_before_speaking = wait_before_speaking
 
         self.get_logger().info('Sending TTS goal...')
         self.tts_voice_clone_client.wait_for_server()
-        send_goal_future = self.tts_voice_clone_client.send_goal_async(goal_msg)
-        rclpy.spin_until_future_complete(self, send_goal_future)
-        goal_handle = send_goal_future.result()
-        if not goal_handle.accepted:
-            self.get_logger().error('TTS goal rejected.')
-            return None
-        result_future = goal_handle.get_result_async()
-        rclpy.spin_until_future_complete(self, result_future)
-        result = result_future.result().result
-        self.get_logger().info(f'TTS result: success={result.success}, message={result.message}')
-        return result
+        self.tts_voice_clone_client.send_goal_async(goal_msg)
+        
 
-    def send_speech_goal_sync(self, continuous=False, publish_partial=False, wave_file=''):
+    def listen(self, continuous=False, publish_partial=False, wav_file=''):
         goal_msg = SpeechDetection.Goal()
         goal_msg.continuous = continuous
         goal_msg.publish_partial = publish_partial
-        goal_msg.wave_file = wave_file
+        goal_msg.wav_file = wav_file
 
         self.get_logger().info('Sending Speech Detection goal...')
         self.speech_client.wait_for_server()
@@ -80,11 +62,11 @@ class GPSR_Demo(Node):
         self.get_logger().info(f'Speech Detection result: {result.final_result.text}')
         return result
 
-    def send_command_speech_goal_sync(self, continuous=False, publish_partial=False, wave_file=''):
+    def listen_command(self, continuous=False, publish_partial=False, wav_file=''):
         goal_msg = SpeechDetection.Goal()
         goal_msg.continuous = continuous
         goal_msg.publish_partial = publish_partial
-        goal_msg.wave_file = wave_file
+        goal_msg.wav_file = wav_file
 
         self.get_logger().info('Sending Command Speech Detection goal...')
         self.command_speech_client.wait_for_server()
@@ -117,89 +99,41 @@ def main(args=None):
     rclpy.init(args=args)
     node = GPSR_Demo()
 
-    command_wave_path = "/scripts/command.wav"
+    command_wav_path = "/scripts/command.wav"
 
     while rclpy.ok():
+        node.tts(text="Please tell me the command")
 
-        # Ask for command
-        tts_result = node.send_tts_goal_sync(
-            text="Please tell me the command",
-            language="",
-            speaker_wave="",
-            wait_before_speaking=0.0
-        )
-
-        # Listen for command
-        command_speech_result = node.send_command_speech_goal_sync(
-            continuous=False,
-            publish_partial=False,
-            wave_file=command_wave_path
-        )
-
-        # If not understood, continue
-        if command_speech_result is None or len(command_speech_result.final_result.text) < 10:
-            # Tell that you clouldn't understand the command
-            tts_result = node.send_tts_goal_sync(
-                text="I couldn't understand your command",
-                language="",
-                speaker_wave="",
-                wait_before_speaking=0.0
-            )
+        result = node.listen_command(wav_file=command_wav_path)
+        if result is None or len(result.final_result.text) < 10:
+            node.tts(text="I couldn't understand your command")
             continue
 
         understood = False
-        question = f"Did you say. {command_speech_result.final_result.text}?"
+        question = f"Did you say. {result.final_result.text}?"
         while rclpy.ok() and not understood:
-
-            # Repeat command and ask for correctnes
-            tts_result = node.send_tts_voice_clone_goal_sync(
+            node.tts_voice_clone(
                 text=question,
-                language="de",
-                speaker_wave=command_wave_path,
-                wait_before_speaking=0.0
+                speaker_wav=command_wav_path,
             )
-
-            # Set grammar to only understand yes or no
-            grammar_response = node.call_set_grammar_sync(grammar_list=["yes", "no"])
-
-            # Listen
-            speech_result = node.send_speech_goal_sync(
-                continuous=False,
-                publish_partial=False,
-                wave_file=""
-            )
-
-            # If not understood, continue
-            if speech_result is None or len(speech_result.final_result.text) < 2:
-                # Tell that you clouldn't it
-                tts_result = node.send_tts_voice_clone_goal_sync(
+            node.call_set_grammar_sync(grammar_list=["yes", "no"])
+            result = node.listen()
+            if result is None or len(result.final_result.text) < 2:
+                node.tts_voice_clone(
                     text="I couldn't understand you",
-                    language="en",
-                    speaker_wave=command_wave_path,
-                    wait_before_speaking=0.0
+                    speaker_wav=command_wav_path,
                 )
                 continue
-            # if understood leave inner loop
             understood = True
-        
-        # if yes in result leave outer loop
-        if "yes" in speech_result.final_result.text:
+        if "yes" in result.final_result.text:
             break
-        # lets try again
-        tts_result = node.send_tts_voice_clone_goal_sync(
+        node.tts_voice_clone(
             text="okay let's try again",
-            language="en",
-            speaker_wave=command_wave_path,
-            wait_before_speaking=0.0
+            speaker_wav=command_wav_path,
         )
-
-
-    # Tell that you will do the command
-    tts_result = node.send_tts_voice_clone_goal_sync(
+    node.tts_voice_clone(
         text="Okay I will do it",
-        language="en",
-        speaker_wave=command_wave_path,
-        wait_before_speaking=0.0
+        speaker_wav=command_wav_path,
     )
 
     node.destroy_node()
